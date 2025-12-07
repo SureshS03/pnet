0015bf5911e413ee8f798fb5391c68e5a27fb31c
SureshS03 <suthamani51@gmail.com>
2025-12-07 21:40:39 +0530

    added start func

--- cmd/vpn.go
+++ cmd/vpn.go
@@ -1,1 +1,23 @@
 package cmd
+
+import (
+	"golang.org/x/crypto/ssh"
+	"bytes"
+	"fmt"
+	"log"
+)
+
+func Start_vpn(client *ssh.Client) (string, error) {
+	session, err := client.NewSession()
+	var out, in bytes.Buffer
+	session.Stdout = &out
+	session.Stdin = &in
+	fmt.Println(CommandMaps[StartVPN])
+	err = session.Run(CommandMaps[StartVPN])
+	if err != nil {
+		fmt.Println("err in runing",err)
+		log.Fatal(err)
+		return "", err
+	}
+	return out.String(), nil
+}
